e357b260f80d68fa6b8ab802b6e66027a0d5a373
username <useremail>
2024-11-05 09:05:34 -0800

    Added Data Validation While loops.

--- CIS110_Pizza_Ordering_Chatbot.py
+++ CIS110_Pizza_Ordering_Chatbot.py
@@ -1,16 +1,29 @@
 print("Hello, my name is Alex your virtual assistant. I will help you order a pizza!")
 print("I am going to ask you a few questions. After typing an answer, press enter.")
 userName = input("\nEnter your name: ")
+while len(userName) == 0:
+    userName = input("Name cannot be blank! Please enter your name: ")
 if userName.lower() == "glory eichler":
     print(f"\nMy creator, {userName}. Pleasure to serve you!")
 else:
     print(f"\nHello, {userName}. Pleasure to meet you!")
 size = input("\nWhat size do you want? Enter small, medium, or large: ")
+while size.lower() not in ["small" , "medium" , "large"]:
+    size = input("Invalid value! Please enter small, medium, or large: ")
 flavor = input("\nEnter the flavor of pizza: ")
+while len(flavor) == 0:
+    flavor = input("Flavor cannot be blank! Please enter a flavor.: ")
 crustType = input("\nWhat type of crust do you want: ")
+while len(crustType) == 0:
+    crustType = input(" Crust type cannot be blank! Please enter crust type: ")
 quantity = input("\nHow many of these do you want to order? Enter a numberic value: ")
+while not quantity.isdigit():
+    quantity = input("Value not recognized. Please enter a numeric value: ")
+    
 quantity = int(quantity)
-method = input("\nIs this carryout or delivery: ")
+method = input("\nIs this carry out or delivery: ")
+while method not in ["carry out" , "delivery"]:
+    method = input("Invalid value! Please enter carry out or delivery: ")
 salesTax = 1.1
 if method.lower() == "delivery":
     deliveryFee = 5
